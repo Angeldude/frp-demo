3fd49fe25f747cf0ad5be3bac118b4967083e1c9
Angel Vanegas <angelman@gmail.com>
2017-08-27 14:26:50 -0400

    minor style fix

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,16 @@ let main = function() {
   let reversed = str => str.split('').reverse().join('')
 
   let functions = [
-  R.compose(reversed, () => text.value),
-  () => "Thank you!",
-  () => "Goodbye!",
-  () => "Onegai Shimasu!",
-  () => "",
-  () => "hello?!"
-]
+    R.compose(reversed, () => text.value),
+    () => "Thank you!",
+    () => "Goodbye!",
+    () => "Onegai Shimasu!",
+    () => "",
+    () => "hello?!"
+  ]
   // OPERATIONS
   let clickEvents = R.map(clicksE, ['reverse', 'thanks', 'goodbye', 'onegai', 'clear', 'hello'])
+
   let buttonEvents = R.map(x => x[0].mapE(x[1]), R.zip(clickEvents, functions))
 
   let singleEventStream = R.map(mergeE, buttonEvents)
