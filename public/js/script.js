f3d4b37d2d55e778e0cfe2d26aee3fb64c5cc250
Angel Vanegas <angelman@gmail.com>
2017-08-29 09:01:19 -0400

    currently trying to make a select, deselect

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 let main = function() {
-
   // SETUP
   let text = document.getElementById('words');
   let reversed = str => str.split('').reverse().join('')
@@ -25,4 +24,31 @@ let main = function() {
   })
 }
 
-window.onload = main;
+let anotherMain = function(){
+  //SETUP
+  let words = ['sel1', 'sel2', 'sel3']
+  // let ideas = R.map(extractIdB, words)
+  // let clicks = R.map(clicksE, words)
+  // let clicks = R.map(clicksE, words)
+  // // let jitter = R.map(snapshotE, clicks)
+  // let one = clicksE('sel1').mapE(x => extractIdB('sel1'))
+  // let test = extractIdB('sel1')
+  // let two = clicksE('sel2').mapE(x => extractIdB('sel2'))
+  // let three = clicksE('sel3').mapE(x => extractIdB('sel3'))
+  let one = clicksE('sel1').mapE(() => 'red')
+  // let merged = mergeE(one, two, three)
+  // merged.mapE(onColors)
+
+  one.mapE(console.log);
+};
+
+function onColors(obj){
+  obj.last.style.color = 'red';
+};
+
+let start = function(){
+  main();
+  anotherMain();
+}
+
+window.onload = start;
